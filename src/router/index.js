810d61eb671729448339cb5ac2b2970853c8f10f
kauaVicto <kauavictor259@gmail.com>
2024-02-29 18:09:19 -0300

    removido bloqueio de auth

--- src/router/index.js
+++ src/router/index.js
@@ -61,7 +61,7 @@ router.beforeEach(async (to, from, next) => {
 		}
 	}
 
-	if (to.meta.requiresAuth && !authStore.isAuth) {
+	if ((to.meta.requiresAuth && !authStore.isAuth) && false) {
 		// Se a rota requer autenticação e o usuário não está autenticado, redirecione para a página de login ou para onde desejar
 		next('/login')
 	} else {
